d41c8db869ec40277a8df85aa805b697993ac5ae
miguelow <miguel.owd@gmail.com>
2023-04-10 11:32:28 +0200

    removed nextauth config bg

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import '../styles/global.css'
-import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
   )
 }
